1d611b0a552dfd76957c5e4f3370844d4a6cbd37
juanmanso <juanmanso@gmail.com>
2021-06-19 20:45:43 -0300

    feat: proper initialization of the project (#6)
    * style: change app icon and splash screen
    
    * chore: [lint] add eslint and prettier
    
    * chore: [lint] add commitlint and contributing guidelines
    
    * chore: [husky] add git hooks
    
    * refactor: project structure
    
    * docs: [github] add templates
    
    * chore: add README and LICENSE files

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  extends: ['@react-native-community', 'airbnb-typescript', 'prettier', 'plugin:react/recommended'],
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
+  ignorePatterns: [],
+  rules: {},
+};
---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,27 @@
+# Progress
+
+- [ ] Task #1
+- [ ] Task #2
+- [ ] Task #3
+
+<br>
+
+Resolves
+Fixes
+Closes
+
+<br>
+
+# Description
+
+Detailed explanation of the feature, what problem aims to solve and why is the change required.
+
+<br>
+
+# Details
+
+## Screenshots
+
+| Android | iOS |
+| :-----: | :-: |
+|         |     |
