f6df5d94697d88a637d30874a131ecec4b302b64
Juan Manso <juanmanso@gmail.com>
2021-06-20 02:28:56 -0300

    feat: [navigation] implement nav container

--- src/AppComponent.tsx
+++ src/AppComponent.tsx
@@ -1,23 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { HomeScreen, QuizScreen, ResultScreen } from './screens';
 
-const AppComponent = () => {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-};
+const Stack = createStackNavigator();
+
+const AppComponent = () => (
+  <>
+    {/* eslint-disable-next-line react/style-prop-object */}
+    <StatusBar style="auto" />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Quiz" component={QuizScreen} />
+        <Stack.Screen name="Result" component={ResultScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  </>
+);
 
 export default AppComponent;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f3f3f3',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
